98f84328d6b5f49551421fb82815393d4a83f8aa
ItzEduhh <drdsebben14@gmail.com>
2025-06-09 22:17:58 -0300

    correcaoEfinalizacaoDosTestesUnitarios

--- tests/unitario.test.js
+++ tests/unitario.test.js
@@ -1,6 +1,15 @@
 // Função que roda todos os testes
 function runUnitTests() {
-  // Testes de soma
+  // Testes de funcionalidade
+
+  function calcularGiros(primogemas) {
+    return Math.floor(primogemas / 160);
+  }
+
+  function quantidadeDePrimogemas() {
+    return Number(document.getElementById("primogemas").value);
+  }
+
 
   testar("UNIT - Quantidade de primogemas para 0 giros", () => {
     if (calcularGiros(159) !== 0) throw new Error("Esperado 0 giros para menos de 160 primogemas");
@@ -11,11 +20,12 @@ function runUnitTests() {
   });
 
   testar("UNIT - Quantidade de primogemas para 2 giros", () => {
-    if (calcularGiros(320) !== 2) throw new Error("Esperado 2 giros para exatas 320 primogemas");
+    if (calcularGiros(320) !== 2) throw new Error("Esperado 2 giros para exatas 160 primogemas");
   });
 
   testar("UNIT - Input de primogemas aceita apenas números", () => {
     const input = quantidadeDePrimogemas();
-      if (!IsNaN(input)) throw new Error("Esperado um número")
+      if (isNaN(input)) throw new Error("Esperado um número")
   });
+
 }
